ead359c5fd1d96229e514bf589706b0b697d1e10
Fergus Kidd <fergus.e.kidd@avanade.com>
2024-02-20 16:44:43 +0000

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,5 +1,3 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "shrill-king-46498",
   url: "https://shrill-king-46498.botics.co",
@@ -7,9 +5,9 @@ export const globalOptions = {
   screenOptions: {
     headerShown: false
   }
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
